857b9091ceb8411eeab4a66f6149b19827f86f7d
llinauer <l.linauer@gmx.at>
2025-03-13 14:26:36 +0100

    Implement Dzenan's mpnet based attack group classifier for single group prediction in mpnet.py

--- attack_group_classifier/mpnet.py
+++ attack_group_classifier/mpnet.py
@@ -1,17 +1,28 @@
+import json
+import torch
+from pathlib import Path
 from attack_group_classifier.config import Config
 from attack_group_classifier.predictor import Predictor
-# import model libraries
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
 
 class Mpnet(Predictor):
-
-    # add huggingface model name (e.g. facebook/bart-large-cnn)
-    # needed for using the modelinfo endpoint
-    model_name = None
+    model_name = "selfconstruct3d/AttackGroup-MPNET"
+    label_file_path = Path(__file__).parent / "label_to_groupid.json"
 
     def __init__(self):
-        # instantiate model here
-        self.model = None
+        with open(self.label_file_path, "r") as f:
+            self.label_to_groupid = json.load(f)
+        self.model = AutoModelForSequenceClassification.from_pretrained(self.model_name, num_labels=len(self.label_to_groupid))
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
 
-    def predict(self):
-        # add inference code here
-        raise NotImplementedError("The class Mpnet must implement the 'predict' method")
+    def predict(self, text: str) -> str:
+        encoding = self.tokenizer(text, truncation=True, padding="max_length", max_length=Config.MAX_TEXT_LENGTH, return_tensors="pt")
+        input_ids = encoding["input_ids"]
+        attention_mask = encoding["attention_mask"]
+
+        outputs = self.model(input_ids=input_ids, attention_mask=attention_mask)
+        logits = outputs.logits
+        predicted_label = torch.argmax(logits, dim=1).item()
+
+        return self.label_to_groupid[str(predicted_label)]
